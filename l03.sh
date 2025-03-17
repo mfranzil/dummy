f15481e6a56b60095b50c912b2477b40ca1a7f29
Matteo Franzil <matteofranzil@gmail.com>
2025-03-17 14:42:54 +0100

    Dummy commit

---
+++ l03.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ~
+wget https://github.com/mfranzil/dummy/raw/refs/heads/master/l03.tar.gz
+tar -xzf l03.tar.gz
+rm l03.tar.gz
+cd linux_demo || echo "Directory not found. Contact the instructor."
+    
