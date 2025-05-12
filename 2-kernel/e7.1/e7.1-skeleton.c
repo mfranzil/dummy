7354a33687cf3baa5f3f79b97060a7f6136af67d
Matteo Franzil <matteofranzil@gmail.com>
2025-05-12 14:32:11 +0200

    Dummy commit

---
+++ 2-kernel/e7.1/e7.1-skeleton.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+int main() {
+    struct sigaction sa;
+
+    // sa.sa_flags = 0;  optional
+    // sigemptyset(&sa.sa_mask);  optional
+
+    // your code
+    
+    return 0;
+}
