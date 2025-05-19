e707e6d428f84f5d5cba9209a551d3780d522a05
Matteo Franzil <matteofranzil@gmail.com>
2025-05-19 10:48:48 +0200

    Dummy commit

---
+++ 2-kernel/e9.1/e9.1-skeleton.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+
+#define N 5
+#define MAX_THINKING_TIME 3
+#define MAX_EATING_TIME 2
+#define SIMULATION_TIME 30
+
+pthread_mutex_t forks[N];
+pthread_t philosophers[N];
+volatile int running = 1;
+
+void *philosopher(void *arg) {
+    // implement your code here
+}
+
+int main() {
+    int ids[N];
+    time_t end_time;
+    
+    srand(time(NULL));
+    
+    printf("Dining Philosophers Problem Simulation\n");
+    printf("Number of philosophers: %d\n", N);
+    printf("Simulation will run for %d seconds\n", SIMULATION_TIME);
+    
+    // implement your code here
+    
+    end_time = time(NULL) + SIMULATION_TIME;
+    while (time(NULL) < end_time) {
+        sleep(1);
+    }
+    
+    running = 0;
+    printf("Simulation ending...\n");
+    
+    // implement your code here
+    
+    printf("Simulation ended\n");
+    return 0;
+}
